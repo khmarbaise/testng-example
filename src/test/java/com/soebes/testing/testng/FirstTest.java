be5b8a3385deddfaa84196fc95e0e21a370104d4
Karl Heinz Marbaise <info@soebes.de>
2013-09-08 16:07:45 +0200

    - Added Listener and invocationCount

--- src/test/java/com/soebes/testing/testng/FirstTest.java
+++ src/test/java/com/soebes/testing/testng/FirstTest.java
@@ -1,12 +1,16 @@
 package com.soebes.testing.testng;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.BeforeClass;
 import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners({ListenerX.class})
 public class FirstTest {
 
 	@BeforeClass
@@ -35,17 +39,20 @@ public class FirstTest {
 	}
 
 	@Test(groups = { Groups.Slow })
-	public void thisIsTheFirstTest() {
+	public void thisIsTheFirstTest() throws InterruptedException {
+		Thread.sleep(TimeUnit.MILLISECONDS.convert(2250, TimeUnit.MILLISECONDS));
 		System.out.println("thisIsTheFirstTest");
 	}
 
 	@Test(groups = { Groups.Medium })
-	public void thisIsTheSecondTest() {
+	public void thisIsTheSecondTest() throws InterruptedException {
+		Thread.sleep(TimeUnit.MILLISECONDS.convert(1500, TimeUnit.MILLISECONDS));
 		System.out.println("thisIsTheSecondTest");
 	}
 
-	@Test(groups = { Groups.Fast })
-	public void thisIsTheThirsTest() {
+	@Test(groups = { Groups.Fast }, invocationCount=5)
+	public void thisIsTheThirsTest() throws InterruptedException {
+		Thread.sleep(TimeUnit.MILLISECONDS.convert(250, TimeUnit.MILLISECONDS));
 		System.out.println("thisIsTheThirdTest");
 	}
 }
---
+++ src/test/java/com/soebes/testing/testng/ListenerX.java
@@ -0,0 +1,44 @@
+package com.soebes.testing.testng;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenerX implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		System.out.println("onTestStart: " + result.getName());
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("onTestSuccess: " + result.getName() + " " + (result.getEndMillis() - result.getStartMillis() + " ms"));
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println("onTestFailure: " + result.getName());
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("onTestSkipped: " + result.getName());
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		System.out.println("onTestFailedButWithinSuccessPercentage: " + result.getName());
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		System.out.println("onStart: " + context.getClass().getName());
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		System.out.println("onFinish: " + context.getClass().getName());
+	}
+
+}
