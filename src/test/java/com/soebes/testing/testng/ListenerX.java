19b42de864debb8dfd40b8742a26d817c2b2aa62
Karl Heinz Marbaise <info@soebes.de>
2013-09-16 08:36:48 +0200

    - Added supplemental output within listener.

--- src/test/java/com/soebes/testing/testng/ListenerX.java
+++ src/test/java/com/soebes/testing/testng/ListenerX.java
@@ -8,37 +8,37 @@ public class ListenerX implements ITestListener {
 
 	@Override
 	public void onTestStart(ITestResult result) {
-		System.out.println("onTestStart: " + result.getName());
+		System.out.println("-> onTestStart: " + result.getName());
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		System.out.println("onTestSuccess: " + result.getName() + " " + (result.getEndMillis() - result.getStartMillis() + " ms"));
+		System.out.println("-> onTestSuccess: " + result.getName() + " " + (result.getEndMillis() - result.getStartMillis() + " ms"));
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		System.out.println("onTestFailure: " + result.getName());
+		System.out.println("-> onTestFailure: " + result.getName());
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		System.out.println("onTestSkipped: " + result.getName());
+		System.out.println("-> onTestSkipped: " + result.getName());
 	}
 
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		System.out.println("onTestFailedButWithinSuccessPercentage: " + result.getName());
+		System.out.println("-> onTestFailedButWithinSuccessPercentage: " + result.getName());
 	}
 
 	@Override
 	public void onStart(ITestContext context) {
-		System.out.println("onStart: " + context.getClass().getName());
+		System.out.println("-> onStart: " + context.getClass().getName());
 	}
 
 	@Override
 	public void onFinish(ITestContext context) {
-		System.out.println("onFinish: " + context.getClass().getName());
+		System.out.println("-> onFinish: " + context.getClass().getName());
 	}
 
 }
